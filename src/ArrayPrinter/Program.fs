b2263bde7ffc929e528bf53a9d97e97af2dbc85b
Christian Steinert <chr.steinert@outlook.com>
2023-11-29 19:13:19 +0100

    Rename to ArrayPrinter.Console and read from Stdin.

--- src/ArrayPrinter/Program.fs
+++ src/ArrayPrinter/Program.fs
@@ -1,14 +1,15 @@
 ﻿
-open ArrayPrinter
+open System
+open System.IO
 
-let myArray = "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ==" |> System.Convert.FromBase64String
+open ArrayPrinter
 
 [<EntryPoint>]
 let main _ =
-    myArray |> printfn "%A"
-    ArrayPrinter.printArray (ArrayPrinter.PrinterOptions.defaults) myArray |> printfn "%s"
-    ArrayPrinter.printArray (ArrayPrinter.PrinterOptions.ocalDefaults) myArray |> printfn "%s"
-    ArrayPrinter.printArray (ArrayPrinter.PrinterOptions.decimalDefaults) myArray |> printfn "%s"
-    ArrayPrinter.printArray (ArrayPrinter.PrinterOptions.binaryDefaults) myArray |> printfn "%s"
-    ArrayPrinter.printArray ({ PrinterOptions.defaults with WithData = false }) myArray |> printfn "%s"
+    use in' = Console.OpenStandardInput()
+    use mem = new MemoryStream()
+    in'.CopyTo mem
+    mem.ToArray ()
+    |> ArrayPrinter.printArray PrinterOptions.defaults
+    |> printfn "%s"
     0
